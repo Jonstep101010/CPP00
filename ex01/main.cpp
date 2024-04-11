38207abfb6b01e39697255a508a8cfd100896f4f
jschwabe <mail@jschwabe.site>
2024-04-11 19:11:18 +0200

    improve user input handling

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,35 +2,31 @@
 #include <iostream>
 #include <limits>
 
-void	loop(PhoneBook my_phonebook)
-{
-	std::string cmd;
+void loop(PhoneBook my_phonebook) {
+  std::string cmd;
 
-	while (true)
-	{
-		std::cout << "Enter your command:\n";
-		std::getline(std::cin, cmd);
-		if (cmd == "EXIT") {
-			return;
-		}
-		if (cmd == "ADD") {
-			my_phonebook.Add();
-		} else if (cmd == "SEARCH") {
-			my_phonebook.List();
-			my_phonebook.Search();
-			std::cin.ignore(
-				std::numeric_limits<std::streamsize>::max(),
-				'\n');
-		} else {
-			std::cout << "Invalid command.\n";
-		}
-	}
+  while (true) {
+    std::cout << "Enter your command:\n";
+    std::getline(std::cin, cmd);
+    if (cmd == "EXIT" || std::cin.eof()) {
+      return;
+    }
+    if (cmd == "ADD") {
+      my_phonebook.Add();
+    } else if (cmd == "SEARCH") {
+      my_phonebook.List();
+      my_phonebook.Search();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    } else {
+      std::cout << "Invalid command.\n";
+    }
+  }
 }
 
 int main() {
-	PhoneBook my_phonebook;
+  PhoneBook my_phonebook;
 
-	loop(my_phonebook);
-    std::cout << "Goodbye!\n";
-	return (0);
+  loop(my_phonebook);
+  std::cout << "Goodbye!\n";
+  return (0);
 }
